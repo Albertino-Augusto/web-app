71312e908d77bbfa5931678683c86821fb290c32
Albertino Augusto <Albertino Augusto@DESKTOP-J7PJB0U>
2019-10-24 21:52:46 +0200

    Segundo commit dentro netbeans

---
+++ src/webapp/WebApp.java
@@ -0,0 +1,15 @@
+
+package webapp;
+
+/**
+ *
+ * @author Albertino Augusto */
+public class WebApp 
+{
+
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+    
+}
